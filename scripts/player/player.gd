5335f0c76fdb2307e6ddac0ddb00fd2f390a954b
spartacoasesino <juan10manuel12@icloud.com>
2026-02-03 18:11:14 +0100

    Jugador básico con cámara 2.5D y salto

---
+++ scripts/player/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody3D
+
+@export var gravity: float = 30.0
+@export var jump_force: float = 10.0
+
+func _physics_process(delta):
+	velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = jump_force
+
+	move_and_slide()
